0b6239fa5b526a0a6b5f9b76537b3a5f6ff75cda
BuildTools <unconfigured@null.spigotmc.org>
2020-04-10 21:18:18 -0500

    [Import Contacts] Created files to avoid future merge conflicts.

---
+++ Connect/Connect/Custom Cells/ContactTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ContactTableViewCell.swift
+//  Connect
+//
+//  Created by Evan Weiss on 4/10/20.
+//  Copyright © 2020 Nikhil Pandeti. All rights reserved.
+//
+
+import UIKit
+
+class ContactTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Connect/Connect/ImportContactsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ImportContactsViewController.swift
+//  Connect
+//
+//  Created by Evan Weiss on 4/10/20.
+//  Copyright © 2020 Nikhil Pandeti. All rights reserved.
+//
+
+import UIKit
+
+class ImportContactsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
