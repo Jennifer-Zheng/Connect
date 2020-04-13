2eba60e3ce544b01cfa7b475fd92f7513a40d66a
BuildTools <unconfigured@null.spigotmc.org>
2020-04-13 16:17:23 -0500

    [Messaging] Created needed files.

---
+++ Connect/Connect/Custom Cells/MessagingTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MessagingTableViewCell.swift
+//  Connect
+//
+//  Created by Evan Weiss on 4/13/20.
+//  Copyright © 2020 Nikhil Pandeti. All rights reserved.
+//
+
+import UIKit
+
+class MessagingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Connect/Connect/MessagingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MessagingViewController.swift
+//  Connect
+//
+//  Created by Evan Weiss on 4/13/20.
+//  Copyright © 2020 Nikhil Pandeti. All rights reserved.
+//
+
+import UIKit
+
+class MessagingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
