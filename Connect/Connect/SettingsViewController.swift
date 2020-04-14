df85f1e5298f1838462502ffe7d493c1b012ff7a
Jennifer Zheng <jennifer.zheng24@gmail.com>
2020-04-13 22:50:14 -0500

    [Edit Profile] Resize profile pic upon upload, alert on too large of an image, and added skeleton code for Settings screen

---
+++ Connect/Connect/SettingsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SettingsViewController.swift
+//  Connect
+//
+//  Created by Jennifer on 4/13/20.
+//  Copyright © 2020 Nikhil Pandeti. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onBackButtonPress(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+
+}
