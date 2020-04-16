2b1a1eea9bd5b8a25fd1d38570caf27f8bd86765
BuildTools <unconfigured@null.spigotmc.org>
2020-04-15 21:29:28 -0500

    [Messaging] Implemented search bar for dummy data.

--- Connect/Connect/MessagingViewController.swift
+++ Connect/Connect/MessagingViewController.swift
@@ -10,33 +10,38 @@ import UIKit
 
 class MessagingViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
     
     // Custom cell identifiers.
     let messagingCellIdentifier = "MessagingCell"
     
-    // Dummy connections array. The structure is bad for testing purposes
+    // Dummy connections array. The structure is for testing purposes and does not represent data on Firebase.
     var connections = [
-        ["user" : "Jon Doe",
+        ["user" : "TestUser 1",
          "lastSender" : "you",
          "lastMessage" : "Hello!"],
-        ["user" : "John Doe",
+        ["user" : "First Last",
         "lastSender" : "other",
         "lastMessage" : "This is a really long message so there should be automatic dots"],
-        ["user" : "Lauren Doe",
+        ["user" : "John Doe",
         "lastSender" : "none",
         "lastMessage" : nil]
     ]
     
+    var filteredConnections: [[String: String?]] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        filteredConnections = connections
+        searchBar.delegate = self
         tableView.dataSource = self
         tableView.delegate = self
     }
     
     // Number of rows needed in the table.
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return connections.count
+        return filteredConnections.count
     }
     
     // Height for each cell.
@@ -46,15 +51,15 @@ class MessagingViewController: UIViewController, UITableViewDelegate, UITableVie
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: messagingCellIdentifier) as! MessagingTableViewCell
-        let otherName = connections[indexPath.row]["user"]!!
+        let otherName = filteredConnections[indexPath.row]["user"]!!
         let otherFirstName = otherName.components(separatedBy: " ")[0]
         cell.otherName?.text = otherName
         //cell.otherProfile?.image
         
         // This is bad code for testing purposes.
-        if (connections[indexPath.row]["lastSender"] == "none") {
+        if (filteredConnections[indexPath.row]["lastSender"] == "none") {
             cell.previewMessage?.text = "You are now connected with " + otherFirstName
-        } else if (connections[indexPath.row]["lastSender"] == "you") {
+        } else if (filteredConnections[indexPath.row]["lastSender"] == "you") {
             cell.previewMessage?.text = "You said: " + connections[indexPath.row]["lastMessage"]!!
         } else {
             cell.previewMessage?.text = connections[indexPath.row]["lastMessage"]!!
@@ -64,5 +69,25 @@ class MessagingViewController: UIViewController, UITableViewDelegate, UITableVie
 
     @IBAction func onBackButtonPress(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    // code to dismiss keyboard when user clicks on background
+
+    func textFieldShouldReturn(textField:UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+}
+
+extension MessagingViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        filteredConnections = searchText.isEmpty ? connections : connections.filter {
+            return $0["user"]!!.range(of: searchText, options: .caseInsensitive, range: nil, locale: nil) != nil
+        }
+        tableView.reloadData()
     }
 }
